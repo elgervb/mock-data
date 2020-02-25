06917f3a2a31a0556cf2469fd7c90564539f69e6
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2020-02-25 22:14:01 +0100

    add exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
+export { isLeapYear } from './assert/is-leap-year';
 export { isNumber } from './assert/is-number';
+
+export { factory } from './generate/factory';
+export { Blueprint, generate } from './generate/mock';
+export { transform } from './generate/transform';
 
 export { randomNumber } from './random/number';
 export { randomString } from './random/string';
-
-export { getMock } from './utils/mock';
+export { randomDate } from './random/date';
+export { randomDay } from './random/date-day';
+export { randomMonth } from './random/date-month';
+export { randomGuid } from './random/guid';

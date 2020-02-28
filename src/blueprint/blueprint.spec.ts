c7b06e2fd92bd5760cc4a64d9327df10e91b8903
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2020-02-28 11:07:29 +0100

    fix code coverage

--- src/blueprint/blueprint.spec.ts
+++ src/blueprint/blueprint.spec.ts
@@ -15,7 +15,29 @@ describe('mock', () => {
       guid: 'guid',
     });
 
-    const mock = from<TestMock>('mock');
+    expect(from<TestMock>('mock')).toBeTruthy();
+  });
+
+});
+
+describe('from', () => {
+
+  const mockName = 'mock';
+
+  beforeEach(() => {
+    register<TestMock>(mockName, {
+      asdf: 'string',
+      count: 'number',
+      guid: 'guid',
+    });
+  });
+
+  it('throws when blueprint could not be found', () => {
+    expect(() => from('non-existing-name')).toThrowError('Cannot find blueprint for name non-existing-name');
+  });
+
+  it('retrieves a registered mock', () => {
+    const mock = from<TestMock>(mockName);
 
     expect(mock).toBeTruthy();
     expect(typeof mock.asdf).toBe('string');

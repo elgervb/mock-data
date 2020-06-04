eebc419a3e560395715b4a066d8755d0c739113f
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2020-06-04 16:29:44 +0200

    random hex color

--- src/random/hex-color.ts
+++ src/random/hex-color.ts
@@ -3,5 +3,9 @@
  * Generate a random hex color, like #f4f4f4
  */
 export function randomHexColor(): string {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const hexChars = 6;
+  let hex = Math.floor(Math.random() * 16777215).toString(16);
+  hex = hex.length === hexChars ? hex : `${hex}0`;
+
+  return `#${hex}`;
 }

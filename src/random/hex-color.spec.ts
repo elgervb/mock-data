dafecc63428767df330145594f67d3c51fe9eef8
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2021-05-20 14:05:30 +0200

    #3 fix code coverage

--- src/random/hex-color.spec.ts
+++ src/random/hex-color.spec.ts
@@ -10,4 +10,13 @@ describe('hex color', () => {
     expect(hex.length).toBe(7);
   });
 
+  it('generates a random hex of 6 chars', () => {
+    jest.spyOn(Math, 'floor').mockReturnValue(189195.0); // 189195.0.toString(16) returns 5 chars
+    const hex = randomHexColor();
+
+    expect(hex.substr(0, 1)).toBe('#');
+    // eslint-disable-next-line: no-magic-numbers
+    expect(hex.length).toBe(7);
+  });
+
 });

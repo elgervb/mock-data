c51fe1a39d95a4a6c9509c7246eca8ed9486fbe7
Elger van Boxtel <elgervb@users.noreply.github.com>
2021-05-20 13:18:53 +0200

    #1 fix geolocation latitude bug (#5)

--- src/random/geolocation.spec.ts
+++ src/random/geolocation.spec.ts
@@ -1,26 +1,30 @@
-import { maxGeoloc, minGeoloc, randomGeolocation } from './geolocation';
+import { GeoType, maxGeolocLat, maxGeolocLong, minGeolocLat, minGeolocLong, randomGeolocation } from './geolocation';
 
 describe('geolocation', () => {
 
   it('should generate a geolocation', () => {
     const result = randomGeolocation();
-    expect(result).toBeLessThanOrEqual(maxGeoloc);
-    expect(result).toBeGreaterThanOrEqual(minGeoloc);
+    expect(result).toBeLessThanOrEqual(maxGeolocLong);
+    expect(result).toBeGreaterThanOrEqual(minGeolocLong);
     expect(result.toString()).toMatch(/([0-9]){1,3}\.([0-9]){0,5}/);
   });
 
   it('should generate a geolocation with custom to and from', () => {
-    const result = randomGeolocation(0, maxGeoloc - 10);
-    expect(result).toBeLessThanOrEqual(maxGeoloc - 10);
+    const result = randomGeolocation(0, maxGeolocLong - 10);
+    expect(result).toBeLessThanOrEqual(maxGeolocLong - 10);
     expect(result).toBeGreaterThanOrEqual(0);
 
-    expect(result).toBeLessThanOrEqual(maxGeoloc);
-    expect(result).toBeGreaterThanOrEqual(minGeoloc);
+    expect(result).toBeLessThanOrEqual(maxGeolocLong);
+    expect(result).toBeGreaterThanOrEqual(minGeolocLong);
     expect(result.toString()).toMatch(/([0-9]){1,3}\.([0-9]){0,5}/);
   });
 
-  it('throws an error if not in range', () => {
-    expect(() => randomGeolocation(minGeoloc, maxGeoloc + 1)).toThrowError(/^randomGeolocation range should be between .*/);
+  it('throws an error if not in range for longitude', () => {
+    expect(() => randomGeolocation(minGeolocLong, maxGeolocLong + 1)).toThrowError(/^randomGeolocation long range should be between .*/);
+  });
+
+  it('throws an error if not in range for latitude', () => {
+    expect(() => randomGeolocation(minGeolocLat, maxGeolocLat + 1, GeoType.lat)).toThrowError(/^randomGeolocation lat range should be between .*/);
   });
 
 });

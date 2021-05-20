d5995f31033fdfd571df1855a5d1254c01bfdc7d
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2021-05-20 12:11:12 +0200

    #1 fix geolocation latitude bug

--- src/random/geolocation.ts
+++ src/random/geolocation.ts
@@ -1,15 +1,28 @@
 import { randomNumber } from './number';
 
-export const minGeoloc = -180;
-export const maxGeoloc = 180;
+export const minGeolocLong = -180;
+export const maxGeolocLong = 180;
+export const minGeolocLat = -90;
+export const maxGeolocLat = 90;
+
+/**
+ * The geolocation type (long or lat)
+ */
+export enum GeoType {
+  long = 'long',
+  lat = 'lat',
+}
 
 /**
  * Renerates a random geo location point
  */
-export function randomGeolocation(from: number = minGeoloc, to: number = maxGeoloc): number {
+export function randomGeolocation(from: number = minGeolocLong, to: number = maxGeolocLong, type: GeoType = GeoType.long): number {
+
+  const minGeoloc = type === GeoType.long ? minGeolocLong : minGeolocLat;
+  const maxGeoloc = type === GeoType.long ? maxGeolocLong : maxGeolocLat;
 
   if (from < minGeoloc || to > maxGeoloc) {
-    throw new Error(`randomGeolocation range should be between ${minGeoloc} and ${maxGeoloc}`);
+    throw new Error(`randomGeolocation ${type} range should be between ${minGeoloc} and ${maxGeoloc}`);
   }
 
   const fractionDigits = 5;
